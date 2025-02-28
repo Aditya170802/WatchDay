3fdafc6d0855e042f4f8e887b8ae8666d5e59cf0
Aditya Ghuraiya <aditya@Adityas-Macbook-Air.local>
2025-02-28 23:10:38 +0530

    Initial Commit

---
+++ WatchDay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchDay
+//
+//  Created by Aditya Ghuraiya on 28/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchDay/WatchDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchDayApp.swift
+//  WatchDay
+//
+//  Created by Aditya Ghuraiya on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
